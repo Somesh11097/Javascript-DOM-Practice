0ed5b447b5112b67806fe4f1e266f85c80dc4045
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2023-05-23 12:46:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+// //Dom mainpulation
+
+// //GetElementByID()
+
+// const title = document.getElementById('main-heading');
+
+// console.log(title);
+
+// //GetElementByClasssName()
+
+// // const list = document.getElementsByClassName('list-items');
+// // console.log(list);
+
+// //getElementByTagName()
+
+// const list = document.getElementsByClassName('li');
+// console.log(list);
+
+// //querySelector()
+
+
+// const container =  document.querySelector('div');
+// console.log(container);
+
+// //querySelectorAll()
+
+// const title =  document.querySelector('#main-heading');
+
+// title.style.color = 'red';
+// console.log(title);
+
+//create element
+
+//modifying attributes and class
+
+//Parent node traversal
+
+let ul = document.querySelector("ul");
+console.log(ul.parentNode);
+console.log(ul.parentElement);
